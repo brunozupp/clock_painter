873e2469c4f7562288bcfc9b808bd704afa3d767
Bruno Noveli <brunimzuppwolf@gmail.com>
2023-01-22 21:53:16 -0300

    Exemplos com círculos

--- lib/pages/circle_examples/circle_examples_page.dart
+++ lib/pages/circle_examples/circle_examples_page.dart
@@ -1,3 +1,6 @@
+import 'package:clock_painter/components/card_component.dart';
+import 'package:clock_painter/pages/circle_examples/examples/circle_example_one_page.dart';
+import 'package:clock_painter/pages/circle_examples/examples/circle_example_two_page.dart';
 import 'package:flutter/material.dart';
     
 class CircleExamplesPage extends StatelessWidget {
@@ -8,9 +11,34 @@ class CircleExamplesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('Circles'),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: _buildCardsPage().map((e) {
+            return Padding(
+              padding: const EdgeInsets.only(
+                bottom: 16,
+              ),
+              child: e,
+            );
+          }).toList(),
+        ),
+      ),
     );
+  }
+
+  List<Widget> _buildCardsPage() {
+    return const [
+      CardComponent(
+        label: "Exemplo 1", 
+        page: CircleExampleOnePage(),
+      ),
+      CardComponent(
+        label: "Exemplo 2", 
+        page: CircleExampleTwoPage(),
+      ),
+    ];
   }
 }
---
+++ lib/pages/circle_examples/examples/circle_example_two_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+    
+class CircleExampleTwoPage extends StatelessWidget {
+
+  const CircleExampleTwoPage({ Key? key }) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Circle Example 2'),
+      ),
+      body: CustomPaint(
+        painter: _ShapePainter(),
+        child: Container(),
+      ),
+    );
+  }
+}
+
+class _ShapePainter extends CustomPainter {
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    
+    final paint = Paint()
+      ..color = Colors.teal
+      ..strokeWidth = 5
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    final path = Path();
+
+    path.addOval(Rect.fromCircle(
+      center: Offset(size.width / 2, size.height / 2), 
+      radius: 100,
+    ));
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
