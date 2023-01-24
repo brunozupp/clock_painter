325478508d6ed3a2b439300bb975dd2789166b94
Bruno Noveli <brunimzuppwolf@gmail.com>
2023-01-23 21:31:36 -0300

    Exemplo de Polígono

---
+++ lib/pages/polygon_examples/examples/polygon_example_one_page.dart
@@ -0,0 +1,69 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+  
+class PolygonExampleOnePage extends StatelessWidget {
+
+  const PolygonExampleOnePage({ Key? key }) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Polygon Example 1'),
+      ),
+      body: CustomPaint(
+        painter: _ShapePainter(
+          sides: 4,
+          radius: 100,
+        ),
+        child: Container(),
+      ),
+    );
+  }
+}
+
+class _ShapePainter extends CustomPainter {
+
+  final double radius;
+  final int sides;
+
+  _ShapePainter({
+    required this.radius,
+    required this.sides,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    final paint = Paint()
+      ..color = Colors.teal
+      ..strokeWidth = 5
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+    
+    final path = Path();
+    
+    final angle = (math.pi * 2) / sides;
+
+    Offset center = Offset(size.width / 2, size.height / 2);
+
+    // startPoint => (100.0, 0.0)
+    Offset startPoint = Offset(radius * math.cos(0.0) , radius * math.sin(0.0));
+
+    path.moveTo(startPoint.dx + center.dx, startPoint.dy + center.dy);
+
+    for(int i = 1; i <= sides; i++) {
+      final double x = radius * math.cos(angle * i) + center.dx;
+      final double y = radius * math.sin(angle * i) + center.dy;
+      path.lineTo(x, y);
+    }
+
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
--- lib/pages/polygon_examples/polygon_examples_page.dart
+++ lib/pages/polygon_examples/polygon_examples_page.dart
@@ -1,16 +1,39 @@
+import 'package:clock_painter/components/card_component.dart';
+import 'package:clock_painter/pages/polygon_examples/examples/polygon_example_one_page.dart';
 import 'package:flutter/material.dart';
     
 class PolygonExamplesPage extends StatelessWidget {
 
   const PolygonExamplesPage({ Key? key }) : super(key: key);
   
-  @override
+    @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('Polygons'),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: _buildCardsPage().map((e) {
+            return Padding(
+              padding: const EdgeInsets.only(
+                bottom: 16,
+              ),
+              child: e,
+            );
+          }).toList(),
+        ),
+      ),
     );
+  }
+
+  List<Widget> _buildCardsPage() {
+    return const [
+      CardComponent(
+        label: "Exemplo 1", 
+        page: PolygonExampleOnePage(),
+      ),
+    ];
   }
 }
