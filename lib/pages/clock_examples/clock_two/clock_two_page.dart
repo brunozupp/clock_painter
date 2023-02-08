c32579e8f830c636e16fdc7d551173c7daffe8dd
Bruno Noveli <brunimzuppwolf@gmail.com>
2023-02-07 21:53:13 -0300

    Retirado imports não usados

--- lib/pages/clock_examples/clock_two/clock_two_page.dart
+++ lib/pages/clock_examples/clock_two/clock_two_page.dart
@@ -7,7 +7,7 @@ class ClockTwoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF2D2F41),
+      backgroundColor: const Color(0xFF2D2F41),
       appBar: AppBar(
         title: const Text("Clock Two"),
       ),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:clock_painter/components/card_component.dart';
 import 'package:clock_painter/pages/circle_examples/circle_examples_page.dart';
 import 'package:clock_painter/pages/clock_examples/clock_examples_page.dart';
-import 'package:clock_painter/pages/clock_examples/clock_one/clock_one_page.dart';
-import 'package:clock_painter/pages/clock_examples/clock_two/clock_two_page.dart';
 import 'package:clock_painter/pages/line_examples/line_examples_page.dart';
 import 'package:clock_painter/pages/polygon_examples/polygon_examples_page.dart';
 import 'package:flutter/material.dart';
